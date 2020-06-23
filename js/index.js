fe4721bdbb179183edd4c1c0816509f338fac38a
Mohammed Juned Ahmed <fuzebooth@gmail.com>
2020-06-23 05:55:47 -0400

    Pushed the Lauch of the site by a week

--- js/index.js
+++ js/index.js
@@ -5,22 +5,22 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
 
   /*** Countdown Timer */
   $("#days")
-  .countdown("2020/05/01", function (event) {
+  .countdown("2020/07/01", function (event) {
       $(this).html(event.strftime('%D <em>days</em>'))
   });
 
   $("#hours")
-  .countdown("2020/05/01", function (event) {
+  .countdown("2020/07/01", function (event) {
       $(this).html(event.strftime('%H <em>hours</em>'))
   });
 
   $("#minutes")
-  .countdown("2020/05/01", function (event) {
+  .countdown("2020/07/01", function (event) {
       $(this).html(event.strftime('%M <em>minutes</em>'))
   });
 
   $("#seconds")
-  .countdown("2020/05/01", function (event) {
+  .countdown("2020/07/01", function (event) {
       $(this).html(event.strftime('%S <em>seconds</em>'))
   });
 
